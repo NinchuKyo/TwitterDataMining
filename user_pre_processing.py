4b778ff746a406723953525bcd0547ce23762f3f
Duong Nguyen <davidnguyenT@gmail.com>
2014-11-30 14:18:53 -0600

    Updated user pre_processing script

--- user_pre_processing.py
+++ user_pre_processing.py
@@ -2,6 +2,13 @@
 
 import csv, json
 
+USER_ID = 'userID'
+USERNAME = 'username'
+FRIEND_COUNT = 'friend_count'
+TWEET_COUNT = 'tweet_count'
+FOLLOWER_COUNT = 'follower_count'
+FAVOURITE_COUNT = 'favourite_count'
+
 class User:
     def __init__(self, userID, username, friend_count, follower_count,
         tweet_count, favourite_count):
@@ -9,6 +16,7 @@ class User:
         self.username = username
         self.friend_count = friend_count
         self.follower_count = follower_count
+        self.tweet_count = tweet_count
         self.favourite_count = favourite_count
 
 # Given the csv row of a user, format into the User class
@@ -16,40 +24,68 @@ class User:
 def process_user(row):
     
     # Assume six attributes obtained from file  
-    userID = row[0]
+    userID = int(row[0])
     username = row[1]
-    friend_count = row[2]
-    follower_count = row[3]
-    tweet_count = row[4]
-    favourite_count = row[5]
+    friend_count = int(row[2])
+    follower_count = int(row[3])
+    tweet_count = int(row[4])
+    favourite_count = int(row[5])
 
     user = User(userID, username, friend_count, follower_count, tweet_count,
         favourite_count)
-    json_user = json.dumps(user.__dict__, sort_keys=True)
-    return json_user
+    return user
+
+# overloaded function for user-data read in JSON format
+def process_user_from_json(user):
+
+    userID = user[USER_ID]
+    username = user[USERNAME]
+    friend_count = user[FRIEND_COUNT]
+    follower_count = user[FOLLOWER_COUNT]
+    tweet_count = user[TWEET_COUNT]
+    favourite_count = user[FAVOURITE_COUNT]
+
+    user = User(userID, username, friend_count, follower_count, tweet_count,
+        favourite_count)
+    return user
 
 def write_file(json_content):
     write_filename = 'users.json'
     
     with open(write_filename, 'w+') as json_file:
-        json_file.write(str(json_content))
+        json_file.write(json_content)
      
 def create_json_from_csv(filename):
 
-    user_json_dict = {}
-    user_json_list = []
+    user_json_list = [] 
 
     with open(filename, 'r') as csvfile:
         csvreader = csv.reader(csvfile)
         for row in csvreader:
             user_json_list.append(process_user(row))
 
-    user_json_dict['Twitter_Users'] = user_json_list
-    write_file(user_json_dict)
+    json_content = json.dumps([user.__dict__ for user in user_json_list],
+            sort_keys=True, indent=4, separators=(',', ': '))
+    write_file(json_content)
+
+def user_decoder(obj):
+    return User(obj[USER_ID], obj[USERNAME], obj[FRIEND_COUNT], obj[FOLLOWER_COUNT],
+        obj[TWEET_COUNT], obj[FAVOURITE_COUNT])
+
+def read_json(filename):
+
+    with open(filename, 'r') as json_file:  
+        user_list = json.load(json_file, object_hook=user_decoder)
+        
+    return user_list
 
 def main():
+
     filename = 'users.csv'
+    json_filename = 'users.json'
+
     create_json_from_csv(filename)
+    user_list = read_json(json_filename) 
 
 if __name__ == "__main__":
     main()
